4ee7b7554a6781e35408e3f6b79cc2e5270d9574
dora(도라) <106212580+dora-101@users.noreply.github.com>
2022-11-19 18:36:54 +0900

    블로그 설정 수정

--- blog.config.js
+++ blog.config.js
@@ -1,17 +1,17 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'limsojung',
+  author: 'limsojung',
+  email: 'limsojung94@gmail.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: 'Quality Assistance Engineer',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  since: 2022, // If leave this empty, current year will be used.
+  postsPerPage: 5,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
